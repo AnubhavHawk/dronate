772c3b61df477d9d17be66280ccb640ff76376f2
AnubhavHawk <48518205+AnubhavHawk@users.noreply.github.com>
2020-05-24 11:36:22 +0530

    Delete insert-info.php

--- includes/insert-info.php
+++
@@ -1,8 +0,0 @@
-<?php
-	print_r($_REQUEST);
-	include('db_connection.php');
-	$description = mysqli_real_escape_string($conn, $_REQUEST['description']);
-	$sql = "INSERT INTO info(user_name, latitude, longitutde, user_mobile, user_email, city, description) VALUES('".$_REQUEST['name']."', '".$_REQUEST['lat']."', '".$_REQUEST['long']."', ".$_REQUEST['mobile'].", '".$_REQUEST['email']."', 'city_name', '$description')";
-	echo "<pre>";
-	echo $sql;
-?>
